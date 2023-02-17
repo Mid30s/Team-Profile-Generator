ef4e91fed11c45eec5f535281dd65a6e12bdcf6d
Gary Gao <gary@GaryMacBook-Pro.gateway>
2023-02-17 16:32:47 +1030

    added test for employee and manager

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,39 @@
+const Employee = require('../lib/Employee');
+
+describe("Employee", () => {
+    describe("Initialization", () => {
+        it("should create an object with a name, id, and email if provided valid arguments", () => {
+            const employee = new Employee("Gary", 1, "gary.yanggao@gmail.com");
+
+            expect(employee.name).toEqual("Gary");
+            expect(employee.id).toEqual(1);
+            expect(employee.email).toEqual("gary.yanggao@gmail.com");
+        });
+    });
+
+    describe("Details", () => {
+        it("should return the name of the employee when calling getName()", () => {
+            const employee = new Employee("Gary", 1, "gary.yanggao@gmail.com");
+    
+            expect(employee.getName()).toEqual("Gary");
+        });
+    
+        it("should return the ID of the employee when calling getId()", () => {
+            const employee = new Employee("Gary", 1, "gary.yanggao@gmail.com");
+    
+            expect(employee.getId()).toEqual(1);
+        });
+    
+        it("should return the email of the employee when calling getEmail()", () => {
+            const employee = new Employee("Gary", 1, "gary.yanggao@gmail.com");
+    
+            expect(employee.getEmail()).toEqual("gary.yanggao@gmail.com");
+        });
+        
+        it("should return the role of the employee when calling getRole()", () => {
+            const employee = new Employee("Gary", 1, "gary.yanggao@gmail.com");
+    
+            expect(employee.getRole()).toEqual("Employee");
+        });
+    });
+});
---
+++ __tests__/Manager.test.js
@@ -0,0 +1,26 @@
+const Manager = require("../lib/Manager");
+
+describe("Manager", () => {
+    describe("Initialization", () => {
+        it("should create an object with a name, id, email, and office number if provided valid arguments", () => {
+            const manager = new Manager("Austin", 2, "austin@email.com", 808);
+
+            expect(manager.name).toEqual("Austin");
+            expect(manager.id).toEqual(2);
+            expect(manager.email).toEqual("austin@email.com");
+            expect(manager.officeNumber).toEqual(808);
+        });
+    });
+
+    describe("Details", () => {
+        it("should return the office number of the manager when calling getOfficeNumber()", () => {
+            const manager = new Manager("Austin", 2, "austin@email.com", 808);
+            expect(manager.getOfficeNumber()).toEqual(808);
+        });
+
+        it("should return the role of the manager when calling getRole()", () => {
+            const manager = new Manager("Austin", 2, "austin@email.com", 808);
+            expect(manager.getRole()).toEqual("Manager");
+        });
+    });
+});
